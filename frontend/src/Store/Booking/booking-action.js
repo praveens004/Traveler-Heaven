ab2eb598335c37545c8d128ae3a9979b7b5fa32f
praveens004 <139880167+praveens004@users.noreply.github.com>
2024-12-24 22:35:40 +0530

    Update booking-action.js

--- frontend/src/Store/Booking/booking-action.js
+++ frontend/src/Store/Booking/booking-action.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { addBooking,setBookingDetails,setBookings } from "./booking-slice";
-
+const BASE_URL = "https://traveler-heaven-backkend.onrender.com";
 export const createBooking =(bookingDate)=>async(dispatch)=>{
     try{
-        const response=await axios.post("/api/v1/rent/user/booking/new",bookingDate);
+        const response=await axios.post(`${BASE_URL}/api/v1/rent/user/booking/new`,bookingDate);
         dispatch(addBooking(response.data.data.booking));
     } catch(error){
         console.error("Error Creating Booking:",error);
@@ -12,7 +12,7 @@ export const createBooking =(bookingDate)=>async(dispatch)=>{
 };
 export const fetchBookingDetails=(bookingId)=>async(dispatch)=>{
     try{
-        const response=await axios.get(`/api/v1/rent/user/booking/${bookingId}`);
+        const response=await axios.get(`${BASE_URL}/api/v1/rent/user/booking/${bookingId}`);
         dispatch(setBookingDetails(response.data.data))
     }catch(error){
         console.error("Error fetching booking details",error)
@@ -21,7 +21,7 @@ export const fetchBookingDetails=(bookingId)=>async(dispatch)=>{
 
 export const fetchUserBookings=()=> async(dispatch)=>{
     try{
-        const response =await axios.get("/api/v1/rent/user/booking")
+        const response =await axios.get(`${BASE_URL}/api/v1/rent/user/booking`)
         dispatch(setBookings(response.data.data.bookings))
     }catch(error){
         console.error("Error fetching bookings:",error);
