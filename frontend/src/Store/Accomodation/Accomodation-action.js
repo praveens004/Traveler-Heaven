d2190c7b29223a85394cbdd3b5ed877fc19613a9
praveens004 <139880167+praveens004@users.noreply.github.com>
2024-12-24 22:37:33 +0530

    Update Accomodation-action.js

--- frontend/src/Store/Accomodation/Accomodation-action.js
+++ frontend/src/Store/Accomodation/Accomodation-action.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 import { accomodationActions } from "./Accomodation-slice";
-
+const BASE_URL = "https://traveler-heaven-backkend.onrender.com";
 export const createAccomodation=(accomodationData)=>async(dispatch)=>{
     try{
         dispatch(accomodationActions.getAccomodationRequest());
-        const response=await axios.post("/api/v1/rent/user/newAccommodation",accomodationData)
+        const response=await axios.post(`${BASE_URL}/api/v1/rent/user/newAccommodation`,accomodationData)
         if(!response){
             throw Error("could not get any accomodation")
         }
@@ -16,7 +16,7 @@ export const createAccomodation=(accomodationData)=>async(dispatch)=>{
 export const getAllAccomodation=()=>async(dispatch)=>{
     try{
         dispatch(accomodationActions.getAccomodationRequest());
-        const {data}=await axios.get("/api/v1/rent/user/myAccommodation");
+        const {data}=await axios.get(`${BASE_URL}/api/v1/rent/user/myAccommodation`);
         const accom=data.data;
         dispatch(accomodationActions.getAccomodation(accom));
     }catch(error){
